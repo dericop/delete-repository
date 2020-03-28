4a874bd3cf9f2487722b18edd9ea55bc4fdf7c9e
Daniel Estiven Rico Posada <danielrico.posada@gmail.com>
2020-03-28 18:38:15 -0500

    UPDATE TEST.JS

--- test.js
+++ test.js
@@ -1,191 +1,110 @@
 'use strict';
 
-const {
-  hasCrypto,
-  mustCall,
-  mustNotCall,
-  skip
-} = require('../common');
-if (!hasCrypto)
-  skip('missing crypto');
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+const assert = require('assert');
+const h2 = require('http2');
 
-const {
-  deepStrictEqual,
-  strictEqual,
-  throws
-} = require('assert');
-const {
-  createSecureServer,
-  createServer,
-  connect
-} = require('http2');
-const Countdown = require('../common/countdown');
+const { PerformanceObserver } = require('perf_hooks');
 
-const { readKey } = require('../common/fixtures');
-
-const key = readKey('agent8-key.pem', 'binary');
-const cert = readKey('agent8-cert.pem', 'binary');
-const ca = readKey('fake-startcom-root-cert.pem', 'binary');
-
-{
-  const server = createSecureServer({ key, cert });
-  server.on('stream', mustCall((stream) => {
-    stream.session.origin('https://example.org/a/b/c',
-      new URL('https://example.com'));
-    stream.respond();
-    stream.end('ok');
-  }));
-  server.on('session', mustCall((session) => {
-    session.origin('https://foo.org/a/b/c', new URL('https://bar.org'));
-
-    // Won't error, but won't send anything
-    session.origin();
-
-    [0, true, {}, []].forEach((input) => {
-      throws(
-        () => session.origin(input),
-        {
-          code: 'ERR_INVALID_ARG_TYPE',
-          name: 'TypeError'
-        }
-      );
-    });
-
-    [new URL('foo://bar'), 'foo://bar'].forEach((input) => {
-      throws(
-        () => session.origin(input),
-        {
-          code: 'ERR_HTTP2_INVALID_ORIGIN',
-          name: 'TypeError'
-        }
-      );
-    });
-
-    ['not a valid url'].forEach((input) => {
-      throws(
-        () => session.origin(input),
-        {
-          code: 'ERR_INVALID_URL',
-          name: 'TypeError'
-        }
-      );
-    });
-    const longInput = `http://foo.bar${'a'.repeat(16383)}`;
-    throws(
-      () => session.origin(longInput),
-      {
-        code: 'ERR_HTTP2_ORIGIN_LENGTH',
-        name: 'TypeError'
+const obs = new PerformanceObserver(common.mustCall((items) => {
+  const entry = items.getEntries()[0];
+  assert.strictEqual(entry.entryType, 'http2');
+  assert.strictEqual(typeof entry.startTime, 'number');
+  assert.strictEqual(typeof entry.duration, 'number');
+  switch (entry.name) {
+    case 'Http2Session':
+      assert.strictEqual(typeof entry.pingRTT, 'number');
+      assert.strictEqual(typeof entry.streamAverageDuration, 'number');
+      assert.strictEqual(typeof entry.streamCount, 'number');
+      assert.strictEqual(typeof entry.framesReceived, 'number');
+      assert.strictEqual(typeof entry.framesSent, 'number');
+      assert.strictEqual(typeof entry.bytesWritten, 'number');
+      assert.strictEqual(typeof entry.bytesRead, 'number');
+      assert.strictEqual(typeof entry.maxConcurrentStreams, 'number');
+      switch (entry.type) {
+        case 'server':
+          assert.strictEqual(entry.streamCount, 1);
+          assert(entry.framesReceived >= 3);
+          break;
+        case 'client':
+          assert.strictEqual(entry.streamCount, 1);
+          assert.strictEqual(entry.framesReceived, 8);
+          break;
+        default:
+          assert.fail('invalid Http2Session type');
       }
-    );
-  }));
+      break;
+    case 'Http2Stream':
+      assert.strictEqual(typeof entry.timeToFirstByte, 'number');
+      assert.strictEqual(typeof entry.timeToFirstByteSent, 'number');
+      assert.strictEqual(typeof entry.timeToFirstHeader, 'number');
+      assert.strictEqual(typeof entry.bytesWritten, 'number');
+      assert.strictEqual(typeof entry.bytesRead, 'number');
+      break;
+    default:
+      assert.fail('invalid entry name');
+  }
+}, 4));
 
-  server.listen(0, mustCall(() => {
-    const originSet = [`https://localhost:${server.address().port}`];
-    const client = connect(originSet[0], { ca });
-    const checks = [
-      ['https://foo.org', 'https://bar.org'],
-      ['https://example.org', 'https://example.com']
-    ];
-
-    const countdown = new Countdown(3, () => {
-      client.close();
-      server.close();
-    });
-
-    client.on('origin', mustCall((origins) => {
-      const check = checks.shift();
-      originSet.push(...check);
-      deepStrictEqual(client.originSet, originSet);
-      deepStrictEqual(origins, check);
-      countdown.dec();
-    }, 2));
-
-    client.request().on('close', mustCall(() => countdown.dec())).resume();
-  }));
-}
-
-// Test automatically sending origin on connection start
+// Should throw if entryTypes are not valid
 {
-  const origins = ['https://foo.org/a/b/c', 'https://bar.org'];
-  const server = createSecureServer({ key, cert, origins });
-  server.on('stream', mustCall((stream) => {
-    stream.respond();
-    stream.end('ok');
-  }));
-
-  server.listen(0, mustCall(() => {
-    const check = ['https://foo.org', 'https://bar.org'];
-    const originSet = [`https://localhost:${server.address().port}`];
-    const client = connect(originSet[0], { ca });
-
-    const countdown = new Countdown(2, () => {
-      client.close();
-      server.close();
-    });
-
-    client.on('origin', mustCall((origins) => {
-      originSet.push(...check);
-      deepStrictEqual(client.originSet, originSet);
-      deepStrictEqual(origins, check);
-      countdown.dec();
-    }));
-
-    client.request().on('close', mustCall(() => countdown.dec())).resume();
-  }));
+  const expectedError = { code: 'ERR_VALID_PERFORMANCE_ENTRY_TYPE' };
+  const wrongEntryTypes = { entryTypes: ['foo', 'bar', 'baz'] };
+  assert.throws(() => obs.observe(wrongEntryTypes), expectedError);
 }
 
-// If return status is 421, the request origin must be removed from the
-// originSet
-{
-  const server = createSecureServer({ key, cert });
-  server.on('stream', mustCall((stream) => {
-    stream.respond({ ':status': 421 });
-    stream.end();
-  }));
-  server.on('session', mustCall((session) => {
-    session.origin('https://foo.org');
-  }));
+obs.observe({ entryTypes: ['http2'] });
 
-  server.listen(0, mustCall(() => {
-    const origin = `https://localhost:${server.address().port}`;
-    const client = connect(origin, { ca });
+const body =
+  '<html><head></head><body><h1>this is some data</h2></body></html>';
 
-    client.on('origin', mustCall((origins) => {
-      deepStrictEqual(client.originSet, [origin, 'https://foo.org']);
-      const req = client.request({ ':authority': 'foo.org' });
-      req.on('response', mustCall((headers) => {
-        strictEqual(headers[':status'], 421);
-        deepStrictEqual(client.originSet, [origin]);
-      }));
-      req.resume();
-      req.on('close', mustCall(() => {
-        client.close();
-        server.close();
-      }));
-    }, 1));
-  }));
+const server = h2.createServer();
+
+// We use the lower-level API here
+server.on('stream', common.mustCall(onStream));
+
+function onStream(stream, headers, flags) {
+  assert.strictEqual(headers[':scheme'], 'http');
+  assert.ok(headers[':authority']);
+  assert.strictEqual(headers[':method'], 'GET');
+  assert.strictEqual(flags, 5);
+  stream.respond({
+    'content-type': 'text/html',
+    ':status': 200
+  });
+  stream.write(body.slice(0, 20));
+  stream.end(body.slice(20));
 }
 
-// Origin is ignored on plain text HTTP/2 connections... server will still
-// send them, but client will ignore them.
-{
-  const server = createServer();
-  server.on('stream', mustCall((stream) => {
-    stream.session.origin('https://example.org',
-      new URL('https://example.com'));
-    stream.respond();
-    stream.end('ok');
+server.on('session', common.mustCall((session) => {
+  session.ping(common.mustCall());
+}));
+
+server.listen(0);
+
+server.on('listening', common.mustCall(() => {
+
+  const client = h2.connect(`http://localhost:${server.address().port}`);
+
+  client.on('connect', common.mustCall(() => {
+    client.ping(common.mustCall());
   }));
-  server.listen(0, mustCall(() => {
-    const client = connect(`http://localhost:${server.address().port}`);
-    client.on('origin', mustNotCall());
-    strictEqual(client.originSet, undefined);
-    const req = client.request();
-    req.resume();
-    req.on('close', mustCall(() => {
-      client.close();
-      server.close();
-    }));
+
+  const req = client.request();
+
+  req.on('response', common.mustCall());
+
+  let data = '';
+  req.setEncoding('utf8');
+  req.on('data', (d) => data += d);
+  req.on('end', common.mustCall(() => {
+    assert.strictEqual(body, data);
   }));
-}
+  req.on('close', common.mustCall(() => {
+    client.close();
+    server.close();
+  }));
+
+}));
